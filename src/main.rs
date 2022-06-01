d8c4f2524cbac5f4cafaa3944ae71bb233437f34
loris <76793979+lorislibra@users.noreply.github.com>
2022-06-01 22:55:43 +0200

    fresh database in tests, db pool, working subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use sqlx::{PgPool, Connection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
@@ -6,8 +7,12 @@ use zero2prod::startup::run;
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    
+    run(listener, connection_pool)?.await
 }
